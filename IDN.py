ab33cee23c8091fdd6ec6589709d88b31646f32d
SharapovAndrey <SharapovAndrey@users.noreply.github.com>
2019-06-14 12:59:16 +0300

    Get device info

---
+++ IDN.py
@@ -0,0 +1,8 @@
+import visa
+rm = visa.ResourceManager()
+#print(rm.list_resources())
+addr = rm.list_resources()[0]
+#(u'USB0::0x0957::0x0A07::MY53001937::INSTR', u'ASRL3::INSTR')
+my_instrument = rm.open_resource(addr)
+print(my_instrument.query('*IDN?'))
+#Agilent Technologies,34411A,MY53001937,2.40-2.40-0.09-46-09
