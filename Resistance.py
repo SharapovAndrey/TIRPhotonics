fa332b713d04b00a5aeb2c061e8bb4a1cdec6375
SharapovAndrey <SharapovAndrey@users.noreply.github.com>
2019-06-14 14:25:11 +0300

    5 points + graph

--- Resistance.py
+++ Resistance.py
@@ -1,4 +1,6 @@
 import visa
+import time
+import matplotlib.pyplot as plt
 rm = visa.ResourceManager()
 addr = rm.list_resources()[0]
 keysight = rm.open_resource(addr)
@@ -7,6 +9,22 @@ keysight = rm.open_resource(addr)
 keysight.write("*rst; status:preset; *cls")
 #print msg on the device screen
 #keysight.write('DISP:TEXT " Skoltech "')
-keysight.write('MEAS:RES?')
-res_val = keysight.read()
-print res_val
+
+#plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+res_val_arr = []
+
+for i in range(5):
+    #request resistance measurement (warning: it is possible to set integration time)
+    keysight.write('MEAS:RES?')
+
+    res_val = keysight.read()
+    res_val_arr.append(res_val)
+    print i, res_val
+    ax = fig.add_subplot(111)
+    ax.scatter(i, res_val, c='b')
+    time.sleep(0.5)
+fig.show()
+
