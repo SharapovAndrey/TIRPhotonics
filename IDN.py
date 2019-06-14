ea768083ddf378bd2fd97e6222ed040829c852ac
SharapovAndrey <SharapovAndrey@users.noreply.github.com>
2019-06-14 13:23:23 +0300

    Alternative query

--- IDN.py
+++ IDN.py
@@ -4,5 +4,11 @@ rm = visa.ResourceManager()
 addr = rm.list_resources()[0]
 #(u'USB0::0x0957::0x0A07::MY53001937::INSTR', u'ASRL3::INSTR')
 my_instrument = rm.open_resource(addr)
+print my_instrument
+
 print(my_instrument.query('*IDN?'))
 #Agilent Technologies,34411A,MY53001937,2.40-2.40-0.09-46-09
+
+#same as the following two strings
+#my_instrument.write('*IDN?')
+#print(my_instrument.read())
